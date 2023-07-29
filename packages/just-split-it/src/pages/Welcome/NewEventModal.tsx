3dea17dde86fbe6df7eb21ce86acc249d21ad324
eliav2 <louski.a@gmail.com>
2023-07-29 15:42:07 +0300

    moved NewEventModal to new file

---
+++ packages/just-split-it/src/pages/Welcome/NewEventModal.tsx
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { addEvent } from '@/utils/firebase/firestore/queries';
+import { FirestoreError } from 'firebase/firestore';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import { DialogActions, DialogContentText, TextField } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+
+interface IEventForm {
+  event: string;
+}
+
+export const NewEventModal = () => {
+  const [open, setOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loadingState, setLoadingState] = useState<'idle' | 'loading'>('idle');
+
+  const eventForm = useForm<IEventForm>({
+    defaultValues: {
+      event: '',
+    },
+  });
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const handleCreate = async (data: IEventForm) => {
+    setLoadingState('loading');
+    await addEvent(data.event)
+      .then(() => {
+        setLoadingState('idle');
+        setOpen(false);
+        eventForm.reset();
+      })
+      .catch((e: FirestoreError) => {
+        console.error(e);
+        setLoadingState('idle');
+        setErrorMessage(e.message);
+      });
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Create new Event
+      </Button>
+      <Dialog open={open} onClose={handleCancel}>
+        <form onSubmit={eventForm.handleSubmit(handleCreate)}>
+          <DialogTitle>New Event</DialogTitle>
+          <DialogContent>
+            <DialogContentText>Provide a name for the new event.</DialogContentText>
+            <Controller
+              name={'event'}
+              control={eventForm.control}
+              render={({ field: { onChange, value }, fieldState: { error }, formState }) => (
+                <TextField
+                  // helperText={error ? error.message : null}
+                  // error={!!error}
+                  size="small"
+                  onChange={onChange}
+                  value={value}
+                  fullWidth
+                  label={'Event'}
+                  autoFocus
+                  margin="dense"
+                  type="text"
+                  variant="standard"
+                  helperText={errorMessage}
+                  error={errorMessage.length > 0}
+                />
+              )}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCancel}>Cancel</Button>
+            <Button type={'submit'}>
+              {loadingState == 'idle' ? 'Create' : <CircularProgress />}
+            </Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    </div>
+  );
+};
--- packages/just-split-it/src/pages/Welcome/Welcome.tsx
+++ packages/just-split-it/src/pages/Welcome/Welcome.tsx
@@ -10,17 +10,8 @@ import rrLogo from './logos/rr.svg';
 import tsLogo from './logos/ts.svg';
 import viteLogo from './logos/vite.svg';
 import { Image } from './styled';
-import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import { DialogActions, DialogContentText, TextField } from '@mui/material';
-import { useState } from 'react';
-import { FirestoreError } from 'firebase/firestore';
-import CircularProgress from '@mui/material/CircularProgress';
-import { addEvent } from '@/utils/firebase/firestore/queries';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { NewEventModal } from '@/pages/Welcome/NewEventModal';
 
 function Welcome() {
   // const [user, loading, error] = useAuthState(fbAuth);
@@ -51,86 +42,4 @@ function Welcome() {
   );
 }
 
-interface IEventForm {
-  event: string;
-}
-
-const NewEventModal = () => {
-  const [open, setOpen] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loadingState, setLoadingState] = useState<'idle' | 'loading'>('idle');
-
-  const eventForm = useForm<IEventForm>({
-    defaultValues: {
-      event: '',
-    },
-  });
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleCancel = () => {
-    setOpen(false);
-  };
-
-  const handleCreate = async (data: IEventForm) => {
-    setLoadingState('loading');
-    await addEvent(data.event)
-      .then(() => {
-        setLoadingState('idle');
-        setOpen(false);
-        eventForm.reset();
-      })
-      .catch((e: FirestoreError) => {
-        console.error(e);
-        console.log(e.message);
-        setLoadingState('idle');
-        setErrorMessage(e.message);
-      });
-  };
-
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Create new Event
-      </Button>
-      <Dialog open={open} onClose={handleCancel}>
-        <form onSubmit={eventForm.handleSubmit(handleCreate)}>
-          <DialogTitle>New Event</DialogTitle>
-          <DialogContent>
-            <DialogContentText>Provide a name for the new event.</DialogContentText>
-            <Controller
-              name={'event'}
-              control={eventForm.control}
-              render={({ field: { onChange, value }, fieldState: { error }, formState }) => (
-                <TextField
-                  // helperText={error ? error.message : null}
-                  // error={!!error}
-                  size="small"
-                  onChange={onChange}
-                  value={value}
-                  fullWidth
-                  label={'Event'}
-                  autoFocus
-                  margin="dense"
-                  type="text"
-                  variant="standard"
-                  helperText={errorMessage}
-                  error={errorMessage.length > 0}
-                />
-              )}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCancel}>Cancel</Button>
-            <Button type={'submit'}>
-              {loadingState == 'idle' ? 'Create' : <CircularProgress />}
-            </Button>
-          </DialogActions>
-        </form>
-      </Dialog>
-    </div>
-  );
-};
 export default Welcome;
