83abb9e84fba2708fee8e52fb9c1c36364c8fcdf
eliav2 <louski.a@gmail.com>
2023-08-02 17:36:50 +0300

    .

--- packages/just-split-it/src/utils/hooks/usePassRef.ts
+++ packages/just-split-it/src/utils/hooks/usePassRef.ts
@@ -4,9 +4,9 @@ import React, { useRef } from 'react';
  * this is used to preserve the ref of the child element if given, and promise that the wrapper
  * component has a valid ref to the child element
  *
- * this is used on wrapper component with single child
- * this hook checks if the child element is passing ref element and
- * if it is, it sets the ref to the given ref,else it creates a new ref and return it
+ * this is used on a wrapper component with single child
+ * this hook checks if the child element is passing a ref element, and
+ * if it is, it sets the ref to the given ref,else it creates a new ref and returns it
  *
  * @param children - child of the component
  */
@@ -16,8 +16,8 @@ export const usePassChildrenRef = <T extends any = any>(children: any): React.Re
 
 /**
  * this is used to preserve the ref of an element if given, and if not given, it creates a new ref.
- * this is useful on component that should pass refs, and also uses the ref internally
- * (if the internal component would count on the parent component to pass ref it might be ended with null)
+ * this is useful on a component that should pass refs, and also uses the ref internally
+ * (if the internal component counts on the parent component to pass ref, it might be ended with null)
  */
 export const usePassRef = <T extends any = any>(ref: React.RefObject<T>): React.RefObject<T> => {
   const nodeRef = useRef<T>(null);
